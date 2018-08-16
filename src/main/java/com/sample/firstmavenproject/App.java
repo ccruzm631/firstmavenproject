9209e51f1b409edd278ba5b36cbe1e5442f7cd15
ccruzm631 <ccruzm631@gmail.com>
2018-08-16 12:09:04 -0500

    Add class and script

--- src/main/java/com/sample/firstmavenproject/App.java
+++ src/main/java/com/sample/firstmavenproject/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Persona persona = new Persona(" Isaias!");
+        System.out.println( "Hello World!" + persona.getName());
     }
 }
---
+++ src/main/java/com/sample/firstmavenproject/Persona.java
@@ -0,0 +1,21 @@
+package com.sample.firstmavenproject;
+
+public class Persona {
+
+	private String name;
+
+	public Persona(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
